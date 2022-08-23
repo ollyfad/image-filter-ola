07fc25478280d79344c2f3bd739c1fb7728f0104
ollyfad <florencefadipe4@gmail.com>
2022-08-23 13:15:59 +0100

    Added a image filter function to server.ts file

--- src/server.ts
+++ src/server.ts
@@ -37,8 +37,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
     }
 
     const filtered_image = await filterImageFromURL(image_url);
-
-    res.status(200).sendFile(filtered_image, () => ) {
+    res.status(200).sendFile(filtered_image, () =>  {
       deleteLocalFiles([filtered_image]);
     });
 
